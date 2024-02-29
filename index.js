3ed0a300146f2aa90b8e39af6ebbb4b23ee776a6
weishengbin <897480227@qq.com>
2024-02-29 17:43:38 +0800

    update index

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// import
+const Web3 = require('web3');
 
-module.exports = random;
+// uri
+const ethereumUri = 'NODE_URI';
+
+// create web3 client
+const web3 = new Web3(new Web3.providers.HttpProvider(ethereumUri));
+
+// the block height
+web3.eth.getBlockNumber().then(blockNumber => {
+    console.log('block height: ', blockNumber);
+}).catch(error => {
+    console.error('get block height error: ', error);
+});
+
+// get balance
+const address = 'YOUR_EVM_ADDRESS';
+
+web3.eth.getBalance(address).then(balance => {
+    console.log('balance: ', web3.utils.fromWei(balance, 'ether'), 'ETH');
+}).catch(error => {
+    console.error('get balance error: ', error);
+});
